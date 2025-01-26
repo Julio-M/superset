d453b8eaec104298fe1a4a8dc1c77cb223341cd9
JULIO-M <mihalijulio@gmail.com>
2025-01-25 22:56:01 -0500

    initi

--- superset/initialization/__init__.py
+++ superset/initialization/__init__.py
@@ -60,7 +60,7 @@ from superset.tags.core import register_sqla_event_listeners
 from superset.utils.core import is_test, pessimistic_connection_handling
 from superset.utils.decorators import transaction
 from superset.utils.log import DBEventLogger, get_event_logger_from_cfg_value
-from flask_mail import Mail, Message
+
 if TYPE_CHECKING:
     from superset.app import SupersetApp
 
